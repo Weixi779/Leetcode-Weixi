29e8e3ca0f937601aae7a5a19b18ba16b5d06959
WeiXi <sunshiwei418@126.com>
2024-07-09 13:35:44 +0800

    feat: 滑动窗口专项初版

--- Leetcode-Weixi/Leetcode-Weixi/Hot100/HotProblem.swift
+++ Leetcode-Weixi/Leetcode-Weixi/Hot100/HotProblem.swift
@@ -47,3 +47,123 @@ extension HotProblem {
         return head.next
     }
 }
+
+
+// - MARK: 树 专项
+
+// 102. 二叉树的层序遍历
+extension HotProblem {
+//    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+//        var result = [[Int]]()
+//        var height = 0
+//        helper(root, height, &result)
+//        return result
+//    }
+//    
+//    func helper(_ node: TreeNode?, _ height: Int, _ grid: inout [[Int]]) {
+//        guard let node = node else { return }
+//        if grid.count <= height { grid.append([]) }
+//        grid[height].append(node.val)
+//        helper(node.left, height + 1,  &grid)
+//        helper(node.right, height + 1 , &grid)
+//    }
+    
+    
+    
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else { return [[Int]]() }
+        var result = [[Int]]()
+        var nodes: [TreeNode] = [root]
+        var children: [TreeNode] = []
+        while !nodes.isEmpty {
+            var value = [Int]()
+            for node in nodes {
+                value.append(node.val)
+                if let left = node.left { children.append(left) }
+                if let right = node.right { children.append(right) }
+            }
+            result.append(value)
+            nodes = children
+            children = [TreeNode]()
+        }
+        return result
+    }
+    
+    public class Node {
+        public var val: Int
+        public var children: [Node]
+        public init(_ val: Int) {
+            self.val = val
+            self.children = []
+        }
+    }
+    
+//    func preorder(_ root: Node?) -> [Int] {
+//        guard let root = root else { return [] }
+//        var result = [Int]()
+//        helper(root, &result)
+//        return result
+//    }
+//    
+//    func helper(_ node: Node?, _ array: inout [Int]) {
+//        guard let node = node else { return }
+//        array.append(node.val)
+//        for child in node.children {
+//            helper(child, &array)
+//        }
+//    }
+    
+    func preorder(_ root: Node?) -> [Int] {
+        guard let root = root else { return [] }
+        var result = [Int]()
+        var queue: [Node] = [root]
+        while !queue.isEmpty {
+            let first = queue.removeFirst()
+            queue = first.children + queue
+            result.append(first.val)
+        }
+        return result
+    }
+}
+
+// - MARK: 滑动窗口专项
+/*
+ 关键字: 连续子数组 最长 / 最短
+ 思想模版:
+    双层while循环
+    while (右指针没到终点) {
+        更新结果
+        while (记过符合当前要求) {
+            判断是否记录结果
+            窗口缩小
+        }
+        窗口扩大
+        (或者使用if 窗口缩小 else 窗口扩大 灵活判断)
+    }
+ */
+
+// - MARK: 3. 无重复字符的最长子串
+extension HotProblem {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        var chars = [Character](s)
+        var charSet = Set<Character>()
+        var left = 0
+        var right = 0
+        var result = 0
+
+        while right < chars.count {
+            // 尝试扩展窗口右边界
+            if !charSet.contains(chars[right]) {
+                charSet.insert(chars[right])
+                right += 1
+                result = max(result, right - left)  // 更新最大长度
+            } else {
+                // 如果遇到重复字符，则移动左边界
+                charSet.remove(chars[left])
+                left += 1
+            }
+        }
+        
+        return result
+    }
+}
--- Leetcode-Weixi/Leetcode-Weixi/main.swift
+++ Leetcode-Weixi/Leetcode-Weixi/main.swift
@@ -9,5 +9,20 @@ import Foundation
 
 //let test = LeetCode2023.February.Solution230201()
 //print(test.decodeMessage("the quick brown fox jumps over the lazy dog", "vkbs bs t suepuv"))
-let test = LeetCode2024.May.Solution240525()
-print(test.findIndices([5,10], 1, 2))
+//let test = LeetCode2024.May.Solution240525()
+//print(test.findIndices([5,10], 1, 2))
+
+let hot = HotProblem()
+
+//let root = TreeNode(3)
+//let node9 = TreeNode(9)
+//let node20 = TreeNode(20)
+//root.left = node9
+//root.right = node20
+//let node15 = TreeNode(15)
+//let node7 = TreeNode(7)
+//node20.left = node15
+//node20.right = node7
+//
+//print(hot.levelOrder(root))
+print(hot.lengthOfLongestSubstring("abcabcbb"))
